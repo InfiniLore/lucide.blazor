1ec3ee8c2e7e5cfae50aea704c8e9b4fd32f5b11
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-01 19:28:34 +0100

    Feat: Initial safety tests

--- tests/Tests.InfiniLore.Lucide/LucideLookupDictionaryTests.cs
+++ tests/Tests.InfiniLore.Lucide/LucideLookupDictionaryTests.cs
@@ -1,10 +1,23 @@
 ﻿// ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
-namespace InfiniLore.Lucide.Data;
+using InfiniLore.Lucide.Data;
+
+namespace Tests.InfiniLore.Lucide;
+
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
-public partial class LucideLookupDictionary {
-    public static int Count => IconsByLucideName.Count;
+public class LucideLookupDictionaryTests {
+    [Test]
+    public async Task IsNotEmpty() {
+        // Arrange
+        
+        // Act
+        var count = LucideLookupDictionary.Count;
+
+        // Assert
+        await Assert.That(count).IsNotZero()
+            .And.IsGreaterThan(1500); // Yes Lucide has more than 1500 icons, but we don't want to test them all at the moment
+    }
 }
--- tests/Tests.InfiniLore.Lucide/LucideServiceTests.cs
+++ tests/Tests.InfiniLore.Lucide/LucideServiceTests.cs
@@ -1,10 +1,25 @@
 ﻿// ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
-namespace InfiniLore.Lucide.Data;
+using InfiniLore.Lucide;
+using Microsoft.AspNetCore.Components;
+
+namespace Tests.InfiniLore.Lucide;
+
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
-public partial class LucideLookupDictionary {
-    public static int Count => IconsByLucideName.Count;
+public class LucideServiceTests {
+    [Test]
+    [Arguments("signature")]
+    public async Task CanFindSvgContent(string iconName) {
+        // Arrange
+
+        // Act
+        MarkupString markup = LucideService.GetIconContent(iconName);
+        string data = markup.Value;
+        
+        // Assert
+        await Assert.That(data).IsNotNullOrWhitespace();
+    }
 }
