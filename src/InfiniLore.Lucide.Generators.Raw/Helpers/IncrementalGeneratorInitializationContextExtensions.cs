982c99324a6753936b1a0ff640b8447a8f228aec
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-05 21:15:32 +0100

    Fix: Revert Copy and change of file checking

--- src/InfiniLore.Lucide.Generators.Raw/Helpers/IncrementalGeneratorInitializationContextExtensions.cs
+++ src/InfiniLore.Lucide.Generators.Raw/Helpers/IncrementalGeneratorInitializationContextExtensions.cs
@@ -12,11 +12,9 @@ namespace InfiniLore.Lucide.Generators.Raw.Helpers;
 // ---------------------------------------------------------------------------------------------------------------------
 public static class IncrementalGeneratorInitializationContextExtensions {
 
-    private const string RegexIconSvgPath = @"icons\\.*\.svg$";
-
     private static IncrementalValuesProvider<LucideSvgFile> SelectLucideSvgFiles(this IncrementalGeneratorInitializationContext context) {
         IncrementalValuesProvider<AdditionalText> files = context.AdditionalTextsProvider
-            .Where(file => Regex.IsMatch(file.Path, RegexIconSvgPath));
+            .Where(file => file.Path.EndsWith(".svg"));
 
         return files
             .Select((file, cancellationToken) => new LucideSvgFile(
