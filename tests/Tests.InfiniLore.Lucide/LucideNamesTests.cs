9a1de92b2bb177c35ad06031f3cad6769dff1f4e
Anna Sas <anna.sas.dev@gmail.com>
2025-04-15 13:19:21 +0200

    Feat: Update lucide-static dependency to v0.488.0
    Upgraded `lucide-static` from version 0.487.0 to 0.488.0 in `package.json` and `package-lock.json`. Adjusted test assertion to reflect the updated icon count in the new version.

--- tests/Tests.InfiniLore.Lucide/LucideNamesTests.cs
+++ tests/Tests.InfiniLore.Lucide/LucideNamesTests.cs
@@ -35,6 +35,6 @@ public class LucideNamesTests {
 
         // Assert
         await Assert.That(count).IsNotZero()
-            .And.IsGreaterThanOrEqualTo(1560); // Yes Lucide has more than 1500 icons, but we don't want to test them all at the moment
+            .And.IsGreaterThanOrEqualTo(1565); // Yes Lucide has more than 1500 icons, but we don't want to test them all at the moment
     }
 }
