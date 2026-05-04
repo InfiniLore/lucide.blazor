97baa784796f21556df00f080485387d3477fd86
Anna Sas <anna.sas.dev@gmail.com>
2026-05-04 13:32:28 +0200

    Chore: Update `ci-release` workflow with improved version bumping logic, refine `Program.cs` for clarity, and adjust target frameworks

--- examples/CodeOfChaosExample.BlazorIcons.Lucide/Program.cs
+++ examples/CodeOfChaosExample.BlazorIcons.Lucide/Program.cs
@@ -1,16 +1,22 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
 using CodeOfChaosExample.BlazorIcons.Lucide.Components;
 
 namespace CodeOfChaosExample.BlazorIcons.Lucide;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
 public class Program {
     public static void Main(string[] args) {
-        var builder = WebApplication.CreateBuilder(args);
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
+        
         builder.Services.AddLucideIcons();
 
-        var app = builder.Build();
+        WebApplication app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment()) {
